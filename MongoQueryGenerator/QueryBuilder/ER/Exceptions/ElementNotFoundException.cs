a7904ae0dc2dacba004eb34dd70823a9e5fc46f8
João Vitor Lopes Cabral <jvitorcabral@gmail.com>
2019-01-12 15:15:42 -0200

    Added ER model structure

---
+++ MongoQueryGenerator/QueryBuilder/ER/Exceptions/ElementNotFoundException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryBuilder.ER.Exceptions
+{
+    /// <summary>
+    /// Exception thrown when a FindByName operation fails
+    /// </summary>
+    public class ElementNotFoundException : Exception
+    {
+        #region Constructor
+        /// <summary>
+        /// Initializes a new instance of ElementNotFoundException
+        /// </summary>
+        /// <param name="Message">Exception Message</param>
+        public ElementNotFoundException( string Message ) : base( Message ) { }
+        #endregion
+    }
+}
---
+++ MongoQueryGenerator/QueryBuilder/ER/Model.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using QueryBuilder.ER.Exceptions;
+
+namespace QueryBuilder.ER
+{
+    /// <summary>
+    /// Represents a ER Model
+    /// </summary>
+    public class Model
+    {
+        #region Properties
+        /// <summary>
+        /// Model name
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// ER elements that compose this model
+        /// </summary>
+        public List<BaseERElement> Elements { get; set; }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Finds an element by name
+        /// </summary>
+        /// <param name="Name">Name to search</param>
+        /// <exception cref="ElementNotFoundException"></exception>
+        /// <returns></returns>
+        public BaseERElement FindByName( string Name )
+        {
+            // Store the element
+            BaseERElement elementFound = Elements.First( E => E.Name == Name );
+
+            if ( elementFound == null )
+            {
+                throw new ElementNotFoundException( string.Format( "Failed to find element [{0}] on model [{1}]", Name, this.Name ) );
+            }
+
+            return elementFound;
+        }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Initialize a new Model instance
+        /// </summary>
+        /// <param name="Name">Model name</param>
+        /// <param name="Element">Elements associated to this model</param>
+        public Model( string Name, List<BaseERElement> Elements )
+        {
+            this.Name = Name;
+            this.Elements = Elements;
+        }
+        #endregion
+    }
+}
