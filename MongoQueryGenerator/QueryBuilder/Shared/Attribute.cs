1ae0213d1fca0adea7e6ad1617faf4b856f7ffe3
João Vitor Lopes Cabral <jvitorcabral@gmail.com>
2019-01-12 22:48:21 -0200

    Added support for attributes to have child attributes.

--- MongoQueryGenerator/QueryBuilder/Shared/Attribute.cs
+++ MongoQueryGenerator/QueryBuilder/Shared/Attribute.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QueryBuilder.Shared.Exceptions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -33,9 +34,63 @@ namespace QueryBuilder.Shared
         /// </summary>
         public string Name { get; set; }
         /// <summary>
-        /// Attribute parent
+        /// Child attributes
         /// </summary>
-        public IDataAttributeParent Parent { get; set; }
+        public List<DataAttribute> Children { get; set; }
+        #endregion
+
+        #region Functions
+        /// <summary>
+        /// Find an attribute by name
+        /// </summary>
+        /// <param name="Name">Name to search for</param>
+        /// <returns>Returns an attribute</returns>
+        public DataAttribute FindByName( string Name )
+        {
+            // First search at level 0
+            DataAttribute FoundAttribute = Children.Find( Attr => Attr.Name == Name );
+
+            if ( FoundAttribute is DataAttribute )
+            {
+                return FoundAttribute;                 
+            }
+
+            // Search through each attribute children
+            foreach ( DataAttribute Attribute in Children )
+            {
+                FoundAttribute = Attribute.FindByName( Name );
+
+                if ( FoundAttribute is DataAttribute )
+                {
+                    return FoundAttribute;
+                }
+            }
+
+            // If reaches this far, it means that the attribute wasn't found
+            throw new AttributeNotFoundException( string.Format( "Couldn't find attribute [{0}] as a child attribute of [{1}]", Name, this.Name ) );
+        }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Initializes a new DataAttribute instance
+        /// </summary>
+        /// <param name="Name">Attribute name</param>
+        public DataAttribute( string Name )
+        {
+            this.Name = Name;
+            Children = new List<DataAttribute>();
+        }
+        /// <summary>
+        /// Initializes a new DataAttribute instance
+        /// </summary>
+        /// <param name="Name">Attribute name</param>
+        /// <param name="Children">Child attributes</param>
+        public DataAttribute( string Name, List<DataAttribute> Children )
+        {
+            this.Name = Name;
+            this.Children = Children;
+        }
         #endregion
     }
 }
