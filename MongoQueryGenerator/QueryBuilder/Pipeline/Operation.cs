16dc8e56c1d637e6c4c2ce6930f418b939091145
João Vitor Lopes Cabral <jvitorcabral@gmail.com>
2019-01-13 03:24:40 -0200

    [WIP] Pipeline structure

---
+++ MongoQueryGenerator/QueryBuilder/Pipeline/Operation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryBuilder.Pipeline
+{
+    /// <summary>
+    /// Defines a query operation (JOIN, PROJECT, etc)
+    /// </summary>
+    public abstract class Operation
+    {
+        #region Properties
+        #endregion
+    }
+}
---
+++ MongoQueryGenerator/QueryBuilder/Pipeline/Pipeline.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryBuilder.Pipeline
+{
+    /// <summary>
+    /// Query generator execution pipeline
+    /// </summary>
+    public class Pipeline
+    {
+        #region Properties
+        /// <summary>
+        /// Available Datasets
+        /// </summary>
+        public List<Dataset> Datasets { get; set; }
+        #endregion
+    }
+}
