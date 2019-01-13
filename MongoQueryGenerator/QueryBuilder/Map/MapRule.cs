0364b20afaa233a2a862e8c05340895c9f6f6fbb
João Vitor Lopes Cabral <jvitorcabral@gmail.com>
2019-01-13 02:05:30 -0200

    Added data mapping structure

---
+++ MongoQueryGenerator/QueryBuilder/Map/MapRule.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using QueryBuilder.ER;
+using QueryBuilder.Mongo;
+
+namespace QueryBuilder.Map
+{
+    /// <summary>
+    /// Defines a mapping rule between ER elements and MongoDB
+    /// </summary>
+    public class MapRule
+    {
+        #region Properties
+        /// <summary>
+        /// ER Element that act as the attribute source
+        /// </summary>
+        public BaseERElement Source { get; set; }
+        /// <summary>
+        /// Target MongoDB collection
+        /// </summary>
+        public Collection Target { get; set; }
+        /// <summary>
+        /// Mapping rules
+        /// </summary>
+        public Dictionary<string, string> Rules { get; set; }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Initialize a new MapRule instance
+        /// </summary>
+        /// <param name="Source"></param>
+        /// <param name="Target"></param>
+        public MapRule( BaseERElement Source, Collection Target )
+        {
+            this.Source = Source;
+            this.Target = Target;
+
+            Rules = new Dictionary<string, string>();
+        }
+        /// <summary>
+        /// Initialize a new MapRule instance
+        /// </summary>
+        /// <param name="Source"></param>
+        /// <param name="Target"></param>
+        /// <param name="Rules"></param>
+        public MapRule( BaseERElement Source, Collection Target, Dictionary<string, string> Rules )
+        {
+            this.Source = Source;
+            this.Target = Target;
+
+            this.Rules = Rules;
+        }
+        #endregion
+    }
+}
---
+++ MongoQueryGenerator/QueryBuilder/Map/ModelMapping.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryBuilder.Map
+{
+    /// <summary>
+    /// Defines the model mapping structure
+    /// </summary>
+    public class ModelMapping
+    {
+        #region Properties
+        /// <summary>
+        /// Map name
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Mapping rules
+        /// </summary>
+        public List<MapRule> Rules { get; set; }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Initialize a new instance of ModelMapping class
+        /// </summary>
+        /// <param name="Name">Map name</param>
+        public ModelMapping( string Name )
+        {
+            this.Name = Name;
+
+            Rules = new List<MapRule>();
+        }
+        /// <summary>
+        /// Initialize a new instance of ModelMapping class
+        /// </summary>
+        /// <param name="Name">Map name</param>
+        /// <param name="Rules">Mapping rules</param>
+        public ModelMapping( string Name, List<MapRule> Rules )
+        {
+            this.Name = Name;
+            this.Rules = Rules;
+        }
+        #endregion
+    }
+}
