10fa2181c83f92f7b67dabc555400002ad3e9ca6
João Vitor Lopes Cabral <jvitorcabral@gmail.com>
2019-06-02 19:12:37 -0300

    Added Many To Many test with multiple entities

--- MongoQueryGenerator/QueryBuilder-Tests/ManyToManyRelationships.cs
+++ MongoQueryGenerator/QueryBuilder-Tests/ManyToManyRelationships.cs
@@ -57,5 +57,50 @@ namespace QueryBuilder.Tests
             // Check if both results are equal
             Assert.IsTrue( JToken.DeepEquals( JToken.Parse( HandcraftedResult ), JToken.Parse( GeneratedResult ) ) );
         }
+        [TestMethod]
+        public void ManyToManyMultipleEntities()
+        {
+            // Asserts if the query result for a simple binary join is equal
+            // to a handcrafted query
+            RequiredDataContainer ModelData = ManyToManyRelationshipsDataProvider.ManyToManyMultipleEntities();
+
+            // Load handcrafted query
+            string HandcraftedQuery = Utils.ReadQueryFromFile( "HandcraftedQueries/personCarInsCompanyManyToMany.js" );
+
+            // Assert if the handcrafted query is not null
+            Assert.IsNotNull( HandcraftedQuery );
+
+            // Prepare query generator
+            RelationshipJoinOperator RJoinOp = new RelationshipJoinOperator( (Entity)ModelData.EntityRelationshipModel.FindByName( "Person" ),
+                (Relationship)ModelData.EntityRelationshipModel.FindByName( "Insurance" ),
+                new List<Entity> { (Entity)ModelData.EntityRelationshipModel.FindByName( "Car" ),
+                    (Entity)ModelData.EntityRelationshipModel.FindByName( "InsCompany" ) },
+                ModelData.ERMongoMapping );
+
+            List<AlgebraOperator> OpList = new List<AlgebraOperator> { RJoinOp };
+            Pipeline pipeline = new Pipeline( OpList );
+            QueryGenerator QueryGen = new QueryGenerator( pipeline )
+            {
+                CollectionName = "Person"
+            };
+
+            string GeneratedQuery = QueryGen.Run();
+
+            // Assert if generated query is not null
+            Assert.IsNotNull( GeneratedQuery );
+
+            // Run Queries
+            QueryRunner Runner = new QueryRunner( "mongodb://localhost:27017", "researchManyToMany" );
+
+            string HandcraftedResult = Runner.GetJSON( HandcraftedQuery );
+            string GeneratedResult = Runner.GetJSON( GeneratedQuery );
+
+            // Check if either result is null
+            Assert.IsNotNull( HandcraftedResult );
+            Assert.IsNotNull( GeneratedResult );
+
+            // Check if both results are equal
+            Assert.IsTrue( JToken.DeepEquals( JToken.Parse( HandcraftedResult ), JToken.Parse( GeneratedResult ) ) );
+        }
     }
 }
--- MongoQueryGenerator/QueryBuilder-Tests/ManyToManyRelationshipsDataProvider.cs
+++ MongoQueryGenerator/QueryBuilder-Tests/ManyToManyRelationshipsDataProvider.cs
@@ -55,7 +55,7 @@ namespace QueryBuilder.Tests
             CarCol.DocumentSchema.AddAttribute( "year" );
 
             MongoDBCollection InsuranceCol = new MongoDBCollection( "Insurance" );
-            InsuranceCol.DocumentSchema.AddAttribute( "insuranceId" );
+            InsuranceCol.DocumentSchema.AddAttribute( "_id" );
             InsuranceCol.DocumentSchema.AddAttribute( "personId" );
             InsuranceCol.DocumentSchema.AddAttribute( "carId" );
             InsuranceCol.DocumentSchema.AddAttribute( "companyId" );
@@ -81,6 +81,92 @@ namespace QueryBuilder.Tests
 
             ModelMapping Map = new ModelMapping( "PersonCarMap", new List<MapRule> { PersonRule,
                 CarRule, InsuranceRule} );
+
+            return new RequiredDataContainer( Model, DBSchema, Map );
+        }
+        /// <summary>
+        /// Generates data to test a Many To Many relationship joining multiple entities
+        /// </summary>
+        /// <returns></returns>
+        public static RequiredDataContainer ManyToManyMultipleEntities()
+        {
+            Entity Person = new Entity( "Person" );
+            Person.AddAttribute( "personId" );
+            Person.AddAttribute( "name" );
+
+            Entity Car = new Entity( "Car" );
+            Car.AddAttribute( "carId" );
+            Car.AddAttribute( "name" );
+            Car.AddAttribute( "year" );
+
+            Entity InsCompany = new Entity( "InsCompany" );
+            InsCompany.AddAttribute( "companyId" );
+            InsCompany.AddAttribute( "name" );
+
+            Relationship Insurance = new Relationship( "Insurance", RelationshipCardinality.ManyToMany );
+            Insurance.AddAttribute( "insuranceId" );
+            Insurance.AddAttribute( "personId" );
+            Insurance.AddAttribute( "carId" );
+            Insurance.AddAttribute( "companyId" );
+
+            RelationshipConnection PersonCarConn = new RelationshipConnection( Person,
+                Person.GetAttribute( "personId" ), Insurance.GetAttribute( "personId" ), Car,
+                Car.GetAttribute( "carId" ), Insurance.GetAttribute( "carId" ) );
+
+            RelationshipConnection PersonInsCompanyConn = new RelationshipConnection( Person,
+                Person.GetAttribute( "personId" ), Insurance.GetAttribute( "personId" ), InsCompany,
+                InsCompany.GetAttribute( "companyId" ), Insurance.GetAttribute( "companyId" ) );
+
+            Insurance.AddRelation( PersonCarConn );
+            Insurance.AddRelation( PersonInsCompanyConn );
+
+            ERModel Model = new ERModel( "PersonCar", new List<BaseERElement> { Person, Car, Insurance, InsCompany } );
+
+            // MongoDB Schema
+            MongoDBCollection PersonCol = new MongoDBCollection( "Person" );
+            PersonCol.DocumentSchema.AddAttribute( "_id" );
+            PersonCol.DocumentSchema.AddAttribute( "name" );
+
+            MongoDBCollection CarCol = new MongoDBCollection( "Car" );
+            CarCol.DocumentSchema.AddAttribute( "_id" );
+            CarCol.DocumentSchema.AddAttribute( "name" );
+            CarCol.DocumentSchema.AddAttribute( "year" );
+
+            MongoDBCollection InsuranceCol = new MongoDBCollection( "Insurance" );
+            InsuranceCol.DocumentSchema.AddAttribute( "_id" );
+            InsuranceCol.DocumentSchema.AddAttribute( "personId" );
+            InsuranceCol.DocumentSchema.AddAttribute( "carId" );
+            InsuranceCol.DocumentSchema.AddAttribute( "companyId" );
+
+            MongoDBCollection InsCompanyCol = new MongoDBCollection( "InsCompany" );
+            InsCompanyCol.DocumentSchema.AddAttribute( "_id" );
+            InsCompanyCol.DocumentSchema.AddAttribute( "name" );
+
+            MongoSchema DBSchema = new MongoSchema( "PersonCarSchema",
+                new List<MongoDBCollection> { PersonCol, CarCol, InsuranceCol, InsCompanyCol } );
+
+            // Map
+            MapRule PersonRule = new MapRule( Person, PersonCol );
+            PersonRule.AddRule( "personId", "_id" );
+            PersonRule.AddRule( "name", "name" );
+
+            MapRule CarRule = new MapRule( Car, CarCol );
+            CarRule.AddRule( "carId", "_id" );
+            CarRule.AddRule( "name", "name" );
+            CarRule.AddRule( "year", "year" );
+
+            MapRule InsuranceRule = new MapRule( Insurance, InsuranceCol );
+            InsuranceRule.AddRule( "insuranceId", "_id" );
+            InsuranceRule.AddRule( "personId", "personId" );
+            InsuranceRule.AddRule( "carId", "carId" );
+            InsuranceRule.AddRule( "companyId", "companyId" );
+
+            MapRule InsCompanyRule = new MapRule( InsCompany, InsCompanyCol );
+            InsCompanyRule.AddRule( "companyId", "_id" );
+            InsCompanyRule.AddRule( "name", "name" );
+
+            ModelMapping Map = new ModelMapping( "PersonCarMap", new List<MapRule> { PersonRule,
+                CarRule, InsuranceRule, InsCompanyRule } );
 
             return new RequiredDataContainer( Model, DBSchema, Map );
         }
