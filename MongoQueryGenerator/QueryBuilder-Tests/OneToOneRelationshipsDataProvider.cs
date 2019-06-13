89c21d5d61f3abcd682e1f4d419b05818f46f75d
João Vitor Lopes Cabral <jvitorcabral@gmail.com>
2019-06-12 22:16:01 -0300

    Added another One To One test to cover mixed embedded / external entities with relationship attributes

--- MongoQueryGenerator/QueryBuilder-Tests/OneToOneRelationshipsDataProvider.cs
+++ MongoQueryGenerator/QueryBuilder-Tests/OneToOneRelationshipsDataProvider.cs
@@ -348,6 +348,76 @@ namespace QueryBuilder.Tests
 
             return new RequiredDataContainer( Model, Schema, Map );
         }
+        /// <summary>
+        /// Generates required data to test a One to One relationship
+        /// joining multiple entities with relationship attribute and mixed embedded/external entities
+        /// </summary>
+        /// <returns></returns>
+        public static RequiredDataContainer OneToOneRelationshipAttributes()
+        {
+            // Create ER Stuff
+            Entity Person = new Entity( "Person" );
+            Person.AddAttribute( "personId" );
+            Person.AddAttribute( "name" );
+            Person.AddAttribute( "insuranceId" );
+
+            Entity Car = new Entity( "Car" );
+            Car.AddAttribute( "name" );
+            Car.AddAttribute( "year" );
+
+            Entity Insurance = new Entity( "Insurance" );
+            Insurance.AddAttribute( "insuranceId" );
+            Insurance.AddAttribute( "name" );
+
+            Relationship HasInsurance = new Relationship( "HasInsurance", RelationshipCardinality.OneToOne );
+            HasInsurance.AddAttribute( "insuranceValue" );
+            RelationshipConnection PersonCar = new RelationshipConnection(
+                Person, Person.GetAttribute( "carId" ), Car, Car.GetAttribute( "name" ) );
+            HasInsurance.AddRelation( PersonCar );
+
+            RelationshipConnection PersonInsurance = new RelationshipConnection(
+                Person, Person.GetAttribute( "insuranceId" ), Insurance, Insurance.GetAttribute( "insuranceId" ) );
+            HasInsurance.AddRelation( PersonInsurance );
+
+            ERModel Model = new ERModel( "PersonCarModel", new List<BaseERElement> { Person, Car, HasInsurance, Insurance } );
+
+            // Create MongoDB schema
+            MongoDBCollection PersonCollection = new MongoDBCollection( "Person" );
+            PersonCollection.DocumentSchema.AddAttribute( "_id" );
+            PersonCollection.DocumentSchema.AddAttribute( "name" );
+            PersonCollection.DocumentSchema.AddAttribute( "car.name" );
+            PersonCollection.DocumentSchema.AddAttribute( "car.year" );
+            PersonCollection.DocumentSchema.AddAttribute( "insuranceId" );
+            PersonCollection.DocumentSchema.AddAttribute( "insuranceValue" );
+
+            MongoDBCollection InsuranceCollection = new MongoDBCollection( "Insurance" );
+            InsuranceCollection.DocumentSchema.AddAttribute( "_id" );
+            InsuranceCollection.DocumentSchema.AddAttribute( "name" );
+
+            MongoSchema Schema = new MongoSchema( "PersonCarSchema", new List<MongoDBCollection> { PersonCollection, InsuranceCollection } );
+
+            // Create Map
+            MapRule PersonRule = new MapRule( Model.FindByName( "Person" ), Schema.FindByName( "Person" ) );
+            PersonRule.AddRule( "personId", "_id" );
+            PersonRule.AddRule( "name", "name" );
+            PersonRule.AddRule( "carId", "carId" );
+            PersonRule.AddRule( "insuranceId", "insuranceId" );
+
+            MapRule CarRule = new MapRule( Model.FindByName( "Car" ), Schema.FindByName( "Person" ) );
+            CarRule.AddRule( "name", "car.name" );
+            CarRule.AddRule( "year", "car.year" );
+
+            MapRule InsuranceRule = new MapRule( Model.FindByName( "Insurance" ), Schema.FindByName( "Insurance" ) );
+            InsuranceRule.AddRule( "insuranceId", "_id" );
+            InsuranceRule.AddRule( "name", "name" );
+
+            MapRule RelationshipRule = new MapRule( Model.FindByName( "HasInsurance" ), Schema.FindByName( "Person" ) );
+            RelationshipRule.AddRule( "insuranceValue", "insuranceValue" );
+
+            ModelMapping Map = new ModelMapping( "PersonCarMap", new List<MapRule> { PersonRule, CarRule, InsuranceRule, RelationshipRule } );
+
+            return new RequiredDataContainer( Model, Schema, Map );
+        }
         #endregion
     }
 }
