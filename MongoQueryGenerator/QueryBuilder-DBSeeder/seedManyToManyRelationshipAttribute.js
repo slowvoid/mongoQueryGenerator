76376f93c1733f571a3176fb45c44fda3696c8a5
João Vitor Lopes Cabral <jvitorcabral@gmail.com>
2019-06-18 19:49:31 -0300

    Added 2 more tests to Many to Many relationships, though they are basically the same thing with additional data to the relationship.

---
+++ MongoQueryGenerator/QueryBuilder-DBSeeder/seedManyToManyRelationshipAttribute.js
@@ -0,0 +1,93 @@
+const faker = require('faker')
+const mongoose = require('mongoose')
+
+let personSchema = new mongoose.Schema({
+    _id: { type: Number },
+    name: { type: String },
+}, { collection: 'Person', versionKey: false })
+
+let carSchema = new mongoose.Schema({
+    _id: { type: Number },
+    name: { type: String },
+    year: { type: Number }
+}, { collection: 'Car', versionKey: false })
+
+let insCompanySchema = new mongoose.Schema({
+    _id: { type: Number },
+    name: { type: String }
+}, { collection: 'InsCompany', versionKey: false })
+
+let insuranceSchema = new mongoose.Schema({
+    _id: { type: Number },
+    personId: { type: Number },
+    carId: { type: Number },
+    companyId: { type: Number },
+    insuranceValue: Number,
+    aRandomValue: String
+}, { collection: 'Insurance', versionKey: false })
+
+let dbConn = mongoose.createConnection('mongodb://localhost/researchManyToManyRelationshipAttributes')
+
+let personModel = dbConn.model('Person', personSchema)
+let carModel = dbConn.model('Car', carSchema)
+let insCompanyModel = dbConn.model('InsCompany', insCompanySchema)
+let insuranceModel = dbConn.model('Insurance', insuranceSchema)
+
+// delete collections before running
+dbConn.dropCollection('Person')
+dbConn.dropCollection('Car')
+dbConn.dropCollection('InsCompany')
+dbConn.dropCollection('Insurance')
+
+// person
+for (let i = 0; i < 10; i++) {
+    let model = new personModel({
+        _id: i + 1,
+        name: faker.name.firstName()
+    })
+
+    model.save()
+    console.log(model)
+}
+
+
+let carModels = ['Astra', 'Celta', 'Fox', 'Fusca', 'Onix', 'Uno', 'Mobi']
+
+// car
+for (let i = 0; i < 10; i++) {
+    let model = new carModel({
+        _id: i + 1,
+        name: carModels[faker.random.number({ min: 0, max: carModels.length - 1 })],
+        year: faker.random.number({min: 2000, max: 2019})
+    })
+
+    model.save()
+    console.log(model)
+}
+
+
+// ins company
+for (let i = 0; i < 10; i++) {
+    let model = new insCompanyModel({
+        _id: i + 1,
+        name: faker.company.companyName()
+    })
+
+    model.save()
+    console.log(model)
+}
+
+// insurance
+for (let i = 0; i < 10; i++) {
+    let model = new insuranceModel({
+        _id: i + 1,
+        personId: faker.random.number({ min: 1, max: 10 }),
+        carId: faker.random.number({ min: 1, max: 10 }),
+        companyId: faker.random.number({ min: 1, max: 10 }),
+        insuranceValue: faker.random.number({min: 10000, max: 300000}),
+        aRandomValue: faker.random.uuid()
+    })
+
+    model.save()
+    console.log(model)
+}
--- MongoQueryGenerator/QueryBuilder-Tests/ManyToManyRelationships.cs
+++ MongoQueryGenerator/QueryBuilder-Tests/ManyToManyRelationships.cs
@@ -102,5 +102,93 @@ namespace QueryBuilder.Tests
             // Check if both results are equal
             Assert.IsTrue( JToken.DeepEquals( JToken.Parse( HandcraftedResult ), JToken.Parse( GeneratedResult ) ) );
         }
+        [TestMethod]
+        public void ManyToManyRelationshipAttributeSingleEntity()
+        {
+            // Asserts if the query result for a simple binary join is equal
+            // to a handcrafted query
+            RequiredDataContainer ModelData = ManyToManyRelationshipsDataProvider.ManyToManyRelationshipAttributeSingleEntity();
+
+            // Load handcrafted query
+            string HandcraftedQuery = Utils.ReadQueryFromFile( "HandcraftedQueries/manyToManyRelationshipAttributes.js" );
+
+            // Assert if the handcrafted query is not null
+            Assert.IsNotNull( HandcraftedQuery );
+
+            // Prepare query generator
+            RelationshipJoinOperator RJoinOp = new RelationshipJoinOperator( (Entity)ModelData.EntityRelationshipModel.FindByName( "Person" ),
+                (Relationship)ModelData.EntityRelationshipModel.FindByName( "Insurance" ), new List<Entity> { (Entity)ModelData.EntityRelationshipModel.FindByName( "Car" ) },
+                ModelData.ERMongoMapping );
+
+            List<AlgebraOperator> OpList = new List<AlgebraOperator> { RJoinOp };
+            Pipeline pipeline = new Pipeline( OpList );
+            QueryGenerator QueryGen = new QueryGenerator( pipeline )
+            {
+                CollectionName = "Person"
+            };
+
+            string GeneratedQuery = QueryGen.Run();
+
+            // Assert if generated query is not null
+            Assert.IsNotNull( GeneratedQuery );
+
+            // Run Queries
+            QueryRunner Runner = new QueryRunner( "mongodb://localhost:27017", "researchManyToManyRelationshipAttributes" );
+
+            string HandcraftedResult = Runner.GetJSON( HandcraftedQuery );
+            string GeneratedResult = Runner.GetJSON( GeneratedQuery );
+
+            // Check if either result is null
+            Assert.IsNotNull( HandcraftedResult );
+            Assert.IsNotNull( GeneratedResult );
+
+            // Check if both results are equal
+            Assert.IsTrue( JToken.DeepEquals( JToken.Parse( HandcraftedResult ), JToken.Parse( GeneratedResult ) ) );
+        }
+        [TestMethod]
+        public void ManyToManyRelationshipAttributesMultipleEntities()
+        {
+            // Asserts if the query result for a simple binary join is equal
+            // to a handcrafted query
+            RequiredDataContainer ModelData = ManyToManyRelationshipsDataProvider.ManyToManyRelationshipAttributeMultipleEntities();
+
+            // Load handcrafted query
+            string HandcraftedQuery = Utils.ReadQueryFromFile( "HandcraftedQueries/manyToManyRelationshipAttributesMultipleEntities.js" );
+
+            // Assert if the handcrafted query is not null
+            Assert.IsNotNull( HandcraftedQuery );
+
+            // Prepare query generator
+            RelationshipJoinOperator RJoinOp = new RelationshipJoinOperator( (Entity)ModelData.EntityRelationshipModel.FindByName( "Person" ),
+                (Relationship)ModelData.EntityRelationshipModel.FindByName( "Insurance" ),
+                new List<Entity> { (Entity)ModelData.EntityRelationshipModel.FindByName( "Car" ),
+                    (Entity)ModelData.EntityRelationshipModel.FindByName( "InsCompany" ) },
+                ModelData.ERMongoMapping );
+
+            List<AlgebraOperator> OpList = new List<AlgebraOperator> { RJoinOp };
+            Pipeline pipeline = new Pipeline( OpList );
+            QueryGenerator QueryGen = new QueryGenerator( pipeline )
+            {
+                CollectionName = "Person"
+            };
+
+            string GeneratedQuery = QueryGen.Run();
+
+            // Assert if generated query is not null
+            Assert.IsNotNull( GeneratedQuery );
+
+            // Run Queries
+            QueryRunner Runner = new QueryRunner( "mongodb://localhost:27017", "researchManyToManyRelationshipAttributes" );
+
+            string HandcraftedResult = Runner.GetJSON( HandcraftedQuery );
+            string GeneratedResult = Runner.GetJSON( GeneratedQuery );
+
+            // Check if either result is null
+            Assert.IsNotNull( HandcraftedResult );
+            Assert.IsNotNull( GeneratedResult );
+
+            // Check if both results are equal
+            Assert.IsTrue( JToken.DeepEquals( JToken.Parse( HandcraftedResult ), JToken.Parse( GeneratedResult ) ) );
+        }
     }
 }
