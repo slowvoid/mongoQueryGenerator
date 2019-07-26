bad8c11e9c0d4addf23e081e365bd7b89072861f
João Vitor Lopes Cabral <jvitorcabral@gmail.com>
2019-07-25 22:12:08 -0300

    Added a new test which joins entities from different relationships (including a computed entity)

--- MongoQueryGenerator/QueryBuilder-Tests/ComputedEntity.cs
+++ MongoQueryGenerator/QueryBuilder-Tests/ComputedEntity.cs
@@ -124,5 +124,65 @@ namespace QueryBuilder.Tests
             // Check if both results are equal
             Assert.IsTrue( JToken.DeepEquals( JToken.Parse( HandcraftedResult ), JToken.Parse( GeneratedResult ) ) );
         }
+        [TestMethod]
+        public void OneToOneComputedEntityMultiple2()
+        {
+            // Asserts if the query result for a relationship join operation is equal
+            // to a handcrafted query
+            RequiredDataContainer ModelData = ComputedEntityDataProvider.OneToOneComputedEntityMultiple2();
+
+            // Load handcrafted query
+            string HandcraftedQuery = Utils.ReadQueryFromFile( "HandcraftedQueries/ceOneToOne-Multiple2.js" );
+
+            // Assert if the handcrafted query is not null
+            Assert.IsNotNull( HandcraftedQuery );
+
+            // Prepare query generator
+            ComputedEntity CarRepairedByGarage = new ComputedEntity( "CarRepairedByGarage",
+                (Entity)ModelData.EntityRelationshipModel.FindByName( "Car" ),
+                (Relationship)ModelData.EntityRelationshipModel.FindByName( "Repaired" ),
+                new List<Entity> {
+                    (Entity)ModelData.EntityRelationshipModel.FindByName( "Garage" ),
+                    (Entity)ModelData.EntityRelationshipModel.FindByName("Supplier")
+                } );
+
+            RelationshipJoinArguments RJoinArgs = new RelationshipJoinArguments(
+                (Relationship)ModelData.EntityRelationshipModel.FindByName( "Drives" ),
+                new List<Entity> { CarRepairedByGarage } );
+
+            RelationshipJoinArguments RJoinArgs2 = new RelationshipJoinArguments(
+                (Relationship)ModelData.EntityRelationshipModel.FindByName( "HasInsurance" ),
+                new List<Entity> { (Entity)ModelData.EntityRelationshipModel.FindByName( "Insurance" ) } );
+
+            RelationshipJoinOperator RJoinOp = new RelationshipJoinOperator(
+                (Entity)ModelData.EntityRelationshipModel.FindByName( "Person" ),
+                new List<RelationshipJoinArguments> { RJoinArgs, RJoinArgs2 },
+                ModelData.ERMongoMapping );
+
+            List<AlgebraOperator> OpList = new List<AlgebraOperator> { RJoinOp };
+            Pipeline pipeline = new Pipeline( OpList );
+            QueryGenerator QueryGen = new QueryGenerator( pipeline )
+            {
+                CollectionName = "Person"
+            };
+
+            string GeneratedQuery = QueryGen.Run();
+
+            // Assert if generated query is not null
+            Assert.IsNotNull( GeneratedQuery );
+
+            // Run Queries
+            QueryRunner Runner = new QueryRunner( "mongodb://localhost:27017", "ceOneToOneMultiple2" );
+
+            string HandcraftedResult = Runner.GetJSON( HandcraftedQuery );
+            string GeneratedResult = Runner.GetJSON( GeneratedQuery );
+
+            // Check if either result is null
+            Assert.IsNotNull( HandcraftedResult );
+            Assert.IsNotNull( GeneratedResult );
+
+            // Check if both results are equal
+            Assert.IsTrue( JToken.DeepEquals( JToken.Parse( HandcraftedResult ), JToken.Parse( GeneratedResult ) ) );
+        }
     }
 }
