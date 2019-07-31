8bf538f0caaf5ccd92d94c7fec264ba1283899f8
João Vitor Lopes Cabral <jvitorcabral@gmail.com>
2019-07-31 18:35:47 -0300

    Added new Many To Many test

--- MongoQueryGenerator/QueryBuilder-Tests/ComputedEntity.cs
+++ MongoQueryGenerator/QueryBuilder-Tests/ComputedEntity.cs
@@ -357,5 +357,61 @@ namespace QueryBuilder.Tests
             // Check if both results are equal
             Assert.IsTrue( JToken.DeepEquals( JToken.Parse( HandcraftedResult ), JToken.Parse( GeneratedResult ) ) );
         }
+        [TestMethod]
+        public void ManyToManyComputedEntityOneToOne()
+        {
+            // Asserts if the query result for a relationship join operation is equal
+            // to a handcrafted query
+            RequiredDataContainer ModelData = ComputedEntityDataProvider.ManyToManyComputedEntity2();
+
+            // Load handcrafted query
+            string HandcraftedQuery = Utils.ReadQueryFromFile( "HandcraftedQueries/ceManyToMany-2.js" );
+
+            // Assert if the handcrafted query is not null
+            Assert.IsNotNull( HandcraftedQuery );
+
+            // Prepare query generator
+            ComputedEntity CarRepairedByGarage = new ComputedEntity( "CarManufacturedBy",
+                (Entity)ModelData.EntityRelationshipModel.FindByName( "Car" ),
+                (Relationship)ModelData.EntityRelationshipModel.FindByName( "ManufacturedBy" ),
+                new List<Entity> {
+                    (Entity)ModelData.EntityRelationshipModel.FindByName( "Manufacturer" )
+                } );
+
+            RelationshipJoinArguments RJoinArgs = new RelationshipJoinArguments(
+                (Relationship)ModelData.EntityRelationshipModel.FindByName( "Owns" ),
+                new List<Entity> {
+                    CarRepairedByGarage } );
+
+            RelationshipJoinOperator RJoinOp = new RelationshipJoinOperator(
+                (Entity)ModelData.EntityRelationshipModel.FindByName( "Person" ),
+                new List<RelationshipJoinArguments> { RJoinArgs },
+                ModelData.ERMongoMapping );
+
+            List<AlgebraOperator> OpList = new List<AlgebraOperator> { RJoinOp };
+            Pipeline pipeline = new Pipeline( OpList );
+            QueryGenerator QueryGen = new QueryGenerator( pipeline )
+            {
+                CollectionName = "Person"
+            };
+
+            string GeneratedQuery = QueryGen.Run();
+
+            // Assert if generated query is not null
+            Assert.IsNotNull( GeneratedQuery );
+
+            // Run Queries
+            QueryRunner Runner = new QueryRunner( "mongodb://localhost:27017", "ceManyToMany2" );
+
+            string HandcraftedResult = Runner.GetJSON( HandcraftedQuery );
+            string GeneratedResult = Runner.GetJSON( GeneratedQuery );
+
+            // Check if either result is null
+            Assert.IsNotNull( HandcraftedResult );
+            Assert.IsNotNull( GeneratedResult );
+
+            // Check if both results are equal
+            Assert.IsTrue( JToken.DeepEquals( JToken.Parse( HandcraftedResult ), JToken.Parse( GeneratedResult ) ) );
+        }
     }
 }
---
+++ MongoQueryGenerator/QueryBuilder-Tests/HandcraftedQueries/ceManyToMany-2.js
@@ -0,0 +1,73 @@
+﻿db.Person.aggregate([
+    {"$lookup": {
+        from: 'Owns',
+        let: {
+            personId: '$_id'
+        },
+        pipeline: [
+            {$match: {
+                $expr: {
+                    $eq: ['$personId', '$$personId']
+                }
+            }},
+            {$lookup: {
+                from: 'Car',
+                let: {
+                    carId: '$carId'
+                },
+                pipeline: [
+                    {$match: {
+                        $expr: {
+                            $eq: ['$_id', '$$carId']
+                        }
+                    }},
+                    {$lookup: {
+                        from: 'Manufacturer',
+                        foreignField: '_id',
+                        localField: 'manufacturerId',
+                        as: 'data_Manufacturer'
+                    }},
+                    {$unwind: '$data_Manufacturer'},
+                    {$addFields: {
+                        Car_carId: '$_id',
+                        Car_model: '$model',
+                        Car_year: '$year',
+                        Car_manufacturerId: '$manufacturerId',
+                        data_ManufacturedBy: [{
+                            Manufacturer_manufacturerId: '$data_Manufacturer._id',
+                            Manufacturer_name: '$data_Manufacturer.name'
+                        }]
+                    }},
+                    {$project: {
+                        _id: false,
+                        model: false,
+                        year: false,
+                        manufacturerId: false,
+                        data_Manufacturer: false
+                    }}
+                ],
+                as: 'data_Car'
+            }},
+            {$unwind: '$data_Car'},
+            {$addFields: {
+                Owns_ownsId: '$_id',
+                Owns_personId: '$personId',
+                Owns_carId: '$carId',
+                Owns_insuranceId: '$insuranceId'
+            }},
+            {$replaceRoot: {
+                newRoot: {
+                    $mergeObjects: ['$data_Car', '$$ROOT']
+                }
+            }},
+            {$project: {
+                _id: false,
+                personId: false,
+                carId: false,
+                insuranceId: false,
+                data_Car: false
+            }}
+        ],
+        as: 'data_Owns'
+    }}
+]).pretty()
