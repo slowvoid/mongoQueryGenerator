3532ccdc24a3ebae63b3b887fb8d0074cb487832
João Vitor Lopes Cabral <jvitorcabral@gmail.com>
2019-09-05 20:49:23 -0300

    Added Select Argument class

---
+++ MongoQueryGenerator/QueryBuilder/Operation/Arguments/SelectArgument.cs
@@ -0,0 +1,39 @@
+﻿using QueryBuilder.Mongo.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryBuilder.Operation.Arguments
+{
+    /// <summary>
+    /// Represents an argument used in the select stage
+    /// </summary>
+    public class SelectArgument
+    {
+        #region Properties
+        /// <summary>
+        /// Entity in which the attributes belong to
+        /// </summary>
+        public QueryableEntity Element { get; set; }
+        /// <summary>
+        /// Attributes and expressions to apply to them
+        /// </summary>
+        public Dictionary<string, BaseExpression> Attributes { get; set; }
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Initialize a new instance of Select Argument class
+        /// </summary>
+        /// <param name="Element"></param>
+        /// <param name="Attributes"></param>
+        public SelectArgument(QueryableEntity Element, Dictionary<string, BaseExpression> Attributes )
+        {
+            this.Element = Element;
+            this.Attributes = Attributes;
+        }
+        #endregion
+    }
+}
--- MongoQueryGenerator/QueryBuilder/Operation/SelectStage.cs
+++ MongoQueryGenerator/QueryBuilder/Operation/SelectStage.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using QueryBuilder.Map;
+using QueryBuilder.Operation.Arguments;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +15,38 @@ namespace QueryBuilder.Operation
     public class SelectStage : AlgebraOperator
     {
         #region Properties
+        /// <summary>
+        /// List of arguments
+        /// </summary>
+        public IEnumerable<SelectArgument> Arguments { get; set; }
+        /// <summary>
+        /// Mapping between ER model and Mongo schema
+        /// </summary>
+        public IModelMap Map { get; set; }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Generate stage code
+        /// </summary>
+        /// <returns></returns>
+        public override AlgebraOperatorResult Run()
+        {
+            return base.Run();
+        }
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Initialize a new instance of SelectStage class
+        /// </summary>
+        /// <param name="Arguments"></param>
+        /// <param name="Map"></param>
+        public SelectStage( IEnumerable<SelectArgument> Arguments, IModelMap Map )
+        {
+            this.Arguments = Arguments;
+            this.Map = Map;
+        }
         #endregion
     }
 }
